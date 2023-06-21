e5643f0ad7287267bf1e6612c93284cb003f07d5
BoshraM <gbboshra@gmail.com>
2023-06-21 16:20:12 +0100

    extra code has been ndeleted and to componenet add for display page

---
+++ client/src/component/User.js
@@ -0,0 +1,10 @@
+export function User() {
+
+	return (
+			<div>
+				<h1>hello world</h1>
+			</div>
+	);
+}
+
+export default User;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,42 +1,13 @@
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-
 import "./Home.css";
-import logo from "./logo.svg";
+import User from "../component/User";
+import MilestoneTable from  "../component/MilestoneTable";
 
 export function Home() {
-	const [message, setMessage] = useState("Loading...");
-
-	useEffect(() => {
-		fetch("/api")
-			.then((res) => {
-				if (!res.ok) {
-					throw new Error(res.statusText);
-				}
-				return res.json();
-			})
-			.then((body) => {
-				setMessage(body.message);
-			})
-			.catch((err) => {
-				console.error(err);
-			});
-	}, []);
 
 	return (
-		<main role="main">
-			<div>
-				<img
-					className="logo"
-					data-qa="logo"
-					src={logo}
-					alt="Just the React logo"
-				/>
-				<h1 className="message" data-qa="message">
-					{message}
-				</h1>
-				<Link to="/about/this/site">About</Link>
-			</div>
+		<main className="main_container" role="main">
+			<User />
+			<MilestoneTable />
 		</main>
 	);
 }
