d6dcc3ce4b550a4129a61170ed723b7e52941ec4
Zobeir-Rigi <rigi.zobir@yahoo.com>
2023-06-24 00:10:51 +0100

    creating context with default values

---
+++ client/src/AppContext.js
@@ -0,0 +1,9 @@
+import { createContext } from "react";
+export const AppContext = createContext({
+	userName: "",
+	setUserName: () => {} ,
+    codewarsData : {} ,
+	setCodewarsData : () => {} ,
+    githubData : {} ,
+    setGitHubData : () => {}
+});
