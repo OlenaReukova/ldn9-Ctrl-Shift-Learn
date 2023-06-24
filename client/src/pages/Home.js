e383d01220b36b6f07d05bfda0a44d94a1acbdee
Zobeir-Rigi <rigi.zobir@yahoo.com>
2023-06-24 03:08:42 +0100

    adding Result to hom comp

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,12 @@
 import "./Home.css";
 import User from "../component/User";
-import MilestoneTable from  "../component/MilestoneTable";
+import Result from "../pages/Result"
 
 export function Home() {
-
 	return (
 		<main className="main_container" role="main">
 			<User />
+			<Result />
 		</main>
 	);
 }
