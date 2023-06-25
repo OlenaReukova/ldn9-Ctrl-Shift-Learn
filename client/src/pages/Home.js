e6930518ff5927cbf20e44045a2cfbf5f1d8bd78
Zobeir-Rigi <rigi.zobir@yahoo.com>
2023-06-25 13:30:22 +0100

    removing Result func from here and expot at first

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,15 +1,10 @@
 import "./Home.css";
 import User from "../component/User";
-import MilestoneTable from  "../component/MilestoneTable";
 
-export function Home() {
-
+export const Home = () => {
 	return (
 		<main className="main_container" role="main">
 			<User />
-			<MilestoneTable />
 		</main>
 	);
-}
-
-export default Home;
+};
