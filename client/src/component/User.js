9d3af17b8e0de0a2c44ebd724d65b22f0339b70f
Olena Reukova <kronverk01@gmail.com>
2023-06-26 00:42:35 +0100

    Import Search Component into User Component
    To run the code minor errors should be fixed :
    1. In Home.js line#2 should be changed by:
     import { User } from "../component/User";
    2. In Search.js line#3 should be changed by:
    import { AppContext } from "../AppContext";

--- client/src/component/User.js
+++ client/src/component/User.js
@@ -1,25 +1,25 @@
 import React, { useState } from "react";
 import "./User.css";
+import { Search } from "../component/Search";
 
-export function User() {
-	const [name, setName] = useState("");
+export const User = () => {
+  const [userName, setUserName] = useState("");
 
-	const handleInputChange = (event) => {
-		setName(event.target.value);
-	};
+  const handleInputChange = (event) => {
+    setUserName(event.target.value);
+  };
 
-	return (
-		<div className="user-container">
-			<label htmlFor="username">Your GitHub User Name</label>
-			<input
-				className="user-input"
-				type="text"
-				id="username"
-				value={name}
-				onChange={handleInputChange}
-			/>
-			<button className="user-button">Search</button>
-		</div>
-	);
-}
-export default User;
+  return (
+    <div className="user-container">
+      <label htmlFor="username">Your GitHub User Name</label>
+      <input
+        className="user-input"
+        type="text"
+        id="username"
+        value={userName}
+        onChange={handleInputChange}
+      />
+      <Search userName={userName} />
+    </div>
+  );
+};
