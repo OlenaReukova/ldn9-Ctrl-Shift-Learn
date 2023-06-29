e9c737203d60ec9f1beb0e873017db1449a8ea21
zobeir-rigi <rigi.zobir@yahoo.com>
2023-06-29 15:15:54 +0100

    removed comment and also changing status 500

--- server/controllers/cohortController.js
+++ server/controllers/cohortController.js
@@ -3,11 +3,11 @@ export const getAllCohorts = async (req, res) => {
     try {
       const q = "SELECT * FROM cohorts";
       const result = await db.query(q);
-      const cohorts = result.rows; // every rows has three columns currently.
+      const cohorts = result.rows;
       res.status(200).json({ cohorts });
     } catch (error) {
       console.error("Error retrieving cohorts:", error);
-      res.status(500).send("Failed, Internal Server Error. Database or table not found");
+      res.status(500).send("Failed, Internal Server Error.");
     }
   };
 
