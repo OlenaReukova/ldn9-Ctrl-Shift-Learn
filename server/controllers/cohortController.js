7d4664eeff737fe73b1b66d03fac3db54cbbd814
zobeir-rigi <rigi.zobir@yahoo.com>
2023-06-29 18:11:27 +0100

    find cohort bY id and create a cohort

--- server/controllers/cohortController.js
+++ server/controllers/cohortController.js
@@ -11,10 +11,40 @@ export const getAllCohorts = async (req, res) => {
     }
   };
 
-export const getCohort = (req, res) => {
-    res.status(200).json("you can write your get function to get one cohort here");
-};
+export const getCohort = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const q = "SELECT * FROM cohorts WHERE id = $1";
+    const result = await db.query(q, [id]);
+    const targetedCohort = result.rows[0];
+    if (targetedCohort) {
+      console.log("Cohort found:", targetedCohort);
+      res.status(200).json({ cohort: targetedCohort, message: "Cohort retrieved successfully" });
+    } else {
+      console.error("Cohort not found.");
+      res.status(404).json({ error: "Cohort not found." });
+    }
+  } catch (error) {
+    console.error("Error retrieving cohort:", error);
+    res.status(500).send("Failed to retrieve cohort.");
+  }
+    };
 
-export const createCohort = (req, res) => {
-    res.status(200).json("you can write your post function to create new cohort here");
+export const createCohort = async (req, res) => {
+  try {
+    const { name, start_date } = req.body; //Destructuring
+    if (!name || !start_date) {
+      return res.status(400).json({ error: "Please provide both Name and start_date."  });
+    }
+    const q = "INSERT INTO cohorts (name, start_date) VALUES ($1, $2)";
+     const result = await db.query(q, [name, start_date]);
+    if (result.rowCount === 1) {
+      res.status(201).json({ message: "New cohort created successfully" });
+    } else {
+      throw new Error("Failed to create new cohort.");
+    }
+  } catch (error) {
+    console.error("Error creating cohort:", error);
+    return res.status(500).send("Failed to create new cohort. Please ensure all required fields are provided in the correct format.");
+  }
 };
