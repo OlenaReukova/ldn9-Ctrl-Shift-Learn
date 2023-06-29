c584d411daeb4b244668bf496691e3c29366a8de
BoshraM <gbboshra@gmail.com>
2023-06-29 15:22:08 +0100

    endpoint to get one tranee information from database

--- server/controllers/traineeController.js
+++ server/controllers/traineeController.js
@@ -1,9 +1,26 @@
+import db from "../db";
+
 export const getAllTrainees = (req, res) => {
     res.status(200).json("hello from trainees route");
 };
 
-export const getTrainee = (req, res) => {
-    res.status(200).json("you can write your get function to get one trainee here");
+export const getTrainee = async (req, res) => {
+    const { traineeId } = req.params;
+
+    try {
+        const q = "SELECT * FROM trainees WHERE id = $1";
+        const result = await db.query(q, [traineeId]);
+        const trainee = result.rows[0];
+
+        if (!trainee) {
+            return res.status(404).json({ error: "Trainee not found" });
+        }
+
+        res.status(200).json({ trainee });
+    } catch (error) {
+        console.error("Error retrieving trainee:", error);
+        res.status(500).send("Failed, Internal Server Error. Database or table not found");
+    }
 };
 
 export const searchTrainee = async (req, res) => {
