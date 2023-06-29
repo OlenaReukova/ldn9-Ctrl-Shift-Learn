a6bea53f5e6377723db476db71f803df4a14a409
BoshraM <gbboshra@gmail.com>
2023-06-29 18:23:39 +0100

    search end point has been deleted

--- server/controllers/traineeController.js
+++ server/controllers/traineeController.js
@@ -19,33 +19,7 @@ export const getTrainee = async (req, res) => {
         res.status(200).json({ trainee });
     } catch (error) {
         console.error("Error retrieving trainee:", error);
-        res.status(500).send("Failed, Internal Server Error. Database or table not found");
-    }
-};
-
-export const searchTrainee = async (req, res) => {
-    const { userName } = req.params;
-
-    try {
-        const codewarsDataResponse = await fetch(`https://www.codewars.com/api/v1/users/${userName}`);
-        if (!codewarsDataResponse.ok) {
-            res.status(404).json({ error: "the serever can not get data, please be sure your userName is correct" });
-        }
-        const codewarsData = await codewarsDataResponse.json();
-
-        const githubDataResponse = await fetch(`https://api.github.com/search/issues?q=is:pr%20author:${userName}%20user:codeyourfuture`);
-        if (!githubDataResponse.ok) {
-            res.status(404).json({ error: "the serever can not get data, please be sure your userName is correct" });
-        }
-        const githubData = await githubDataResponse.json();
-
-        res.status(200).json({
-            codewarsData,
-            githubData,
-        });
-    } catch (err) {
-        console.error(err);
-        res.status(500).json("Failed, Internal Server Error");
+        res.status(500).send("Failed, Internal Server Error");
     }
 };
 
