61a423214129f65f4ac287ba5a0b3511c856a600
BoshraM <gbboshra@gmail.com>
2023-06-30 23:59:22 +0100

    a table component created for list of cohorts

---
+++ client/src/component/CohortsTable.js
@@ -0,0 +1,18 @@
+export const CohortsTable = () => {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>name</th>
+                    <th>view</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>LDN9</td>
+                    <td> <button>view</button></td>
+                </tr>
+            </tbody>
+        </table>
+    );
+};
