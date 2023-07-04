0fcec4281d01265887f0f44bf95dde3a319e1f71
BoshraM <gbboshra@gmail.com>
2023-07-04 12:47:41 +0100

    table created to show all the list of Cohorts

--- client/src/component/CohortsTable.js
+++ client/src/component/CohortsTable.js
@@ -1,18 +1,20 @@
+import { Button } from "./Button";
+
 export const CohortsTable = () => {
-    return (
-        <table>
-            <thead>
-                <tr>
-                    <th>name</th>
-                    <th>view</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>LDN9</td>
-                    <td> <button>view</button></td>
-                </tr>
-            </tbody>
-        </table>
-    );
+	return (
+		<table>
+			<thead>
+				<tr>
+					<th>name</th>
+					<th>view</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>LDN9</td>
+					<td> <Button title={"view"} /></td>
+				</tr>
+			</tbody>
+		</table>
+	);
 };
