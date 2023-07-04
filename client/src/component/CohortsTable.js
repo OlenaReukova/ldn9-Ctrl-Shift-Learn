e4b067a953d3f68c6f6c3d4fbe10712dc0b324a1
BoshraM <gbboshra@gmail.com>
2023-07-04 17:37:39 +0100

    cohorts table and getting data for cohorts added

--- client/src/component/CohortsTable.js
+++ client/src/component/CohortsTable.js
@@ -1,20 +1,33 @@
+import { Link } from "react-router-dom";
 import { Button } from "./Button";
 
-export const CohortsTable = () => {
-	return (
-		<table>
-			<thead>
-				<tr>
-					<th>name</th>
-					<th>view</th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr>
-					<td>LDN9</td>
-					<td> <Button title={"view"} /></td>
-				</tr>
-			</tbody>
-		</table>
-	);
+export const CohortsTable = ({ cohortsData }) => {
+  return (
+    <div>
+      {cohortsData.length > 0 ? (
+        <table>
+          <thead>
+            <tr>
+              <th>Cohort Name</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {cohortsData.map((cohort) => (
+              <tr key={cohort.id}>
+                <td>{cohort.name}</td>
+                <td>
+                  <Link to={`/Cohorts/${cohort.name}`}>
+                    <Button title={"view"} />
+                  </Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <p>No cohorts found.</p>
+      )}
+    </div>
+  );
 };
--- client/src/pages/Cohorts.js
+++ client/src/pages/Cohorts.js
@@ -1,11 +1,33 @@
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Button } from "../component/Button";
 import { CohortsTable } from "../component/CohortsTable";
 
 export const Cohorts = () => {
+	const [cohortsData, setCohortsData] = useState([]);
+
+	useEffect(() => {
+		const fetchAllCohorts = async () => {
+
+			try {
+				const response = await fetch("http://localhost:3000/api/cohorts");
+				const data = await response.json();
+				if (response.status === 200) {
+					setCohortsData(data.cohorts);
+				} else {
+					throw new Error("Failed to fetch cohorts");
+				}
+			} catch (error) {
+				console.error("Error fetching cohorts:", error);
+			}
+		};
+		fetchAllCohorts();
+	}, []);
+
 	return (
 		<main>
-			<CohortsTable />
+			<h1>List of Cohorts</h1>
+			<CohortsTable cohortsData={cohortsData} />
 			<Link to="/admin/cohorts/addcohort">
 				<Button title="Add New Cohort" />
 			</Link>
