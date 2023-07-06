c0ae8caf93faf36960371d5f60f16ed51d8e85c9
zobeir-rigi <rigi.zobir@yahoo.com>
2023-07-06 18:43:52 +0100

    created updateTrainee page

--- client/src/pages/GetCohortById.js
+++ client/src/pages/GetCohortById.js
@@ -28,8 +28,8 @@ export const GetCohortById = () => {
 				<table>
 					<thead>
 						<tr>
-							<th>Full Name</th>
 							<th>Github User Name</th>
+							<th>Full Name</th>
 							<th>Pull Requestes</th>
 							<th>Codewars</th>
 							<th>Options</th>
@@ -38,13 +38,13 @@ export const GetCohortById = () => {
 					<tbody>
 						{trainees.map((trainee) => (
 							<tr key={trainee.id}>
-								<td>{trainee.full_name}</td>
 								<td>{trainee.github_user_name}</td>
+								<td>{trainee.full_name}</td>
 								<td>25 out of 26</td>
 								<td> 5ku</td>
 								<td>
 									<span>
-									<Link className="link" to={`/admin/cohorts/${id}/updateTrainee`}>Edit</Link>
+									<Link className="link" to={`/admin/cohorts/${id}/updateTrainee/${trainee.id}`}>Edit</Link>
 									</span>
 									<span>
 										<Link className="link">View</Link>
---
+++ client/src/pages/UpdateTrainee.js
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { Button } from "../component/Button";
+import "../pages/addTrainee.css";
+export const UpdateTrainee = () => {
+	const [fullname, setFullName] = useState("");
+	const [githubUserName, setGithubUserName] = useState("");
+	const [cohortId, setCohortId] = useState("");
+	const { id } = useParams();
+	console.log(id);
+	const navigate = useNavigate();
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			const response = await fetch(`/api/trainees/${id}`, {
+				method: "PUT",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({
+					github_user_name: githubUserName,
+					full_name: fullname,
+					cohort_id: cohortId,
+				}),
+			});
+
+			if (!response.ok) {
+				throw new Error("couldn't to update trainee");
+			}
+
+			navigate(-1);
+		} catch (error) {
+			console.error("Error updating trainee:", error);
+		}
+	};
+
+	return (
+		<div className="addTrainee">
+			<h2>Update Trainee's details</h2>
+			<form onSubmit={handleSubmit}>
+				<label htmlFor="githubUsername">Github User Name </label>
+				<input
+					type="text"
+					placeholder="Github User Name"
+					value={githubUserName}
+					onChange={(e) => setGithubUserName(e.target.value)}
+				/>
+				<label htmlFor="fullName">Full Name </label>
+				<input
+					type="text"
+					placeholder="Full Name"
+					value={fullname}
+					onChange={(e) => setFullName(e.target.value)}
+				/>
+				<label htmlFor="cohortId">Cohort Id </label>
+				<input
+					type="number"
+					placeholder="cohort Id"
+					value={cohortId}
+					onChange={(e) => setCohortId(e.target.value)}
+				/>
+				<Button title={"Update"} />
+			</form>
+		</div>
+	);
+};
