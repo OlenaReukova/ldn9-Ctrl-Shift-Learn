1189e9e9289a8fb96ec52168c3eeef1a65f5ea8d
zobeir-rigi <rigi.zobir@yahoo.com>
2023-07-06 02:09:59 +0100

    added edit & view link to options th

--- client/src/pages/GetCohortById.js
+++ client/src/pages/GetCohortById.js
@@ -32,6 +32,7 @@ export const GetCohortById = () => {
 							<th>Github User Name</th>
 							<th>Pull Requestes</th>
 							<th>Codewars</th>
+							<th>Options</th>
 						</tr>
 					</thead>
 					<tbody>
@@ -41,6 +42,14 @@ export const GetCohortById = () => {
 								<td>{trainee.github_user_name}</td>
 								<td>25 out of 26</td>
 								<td> 5ku</td>
+								<td>
+									<span>
+									<Link className="link" to={`/admin/cohorts/${id}/updateTrainee`}>Edit</Link>
+									</span>
+									<span>
+										<Link className="link">View</Link>
+									</span>
+								</td>
 							</tr>
 						))}
 					</tbody>
