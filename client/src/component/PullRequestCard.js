5640c58f80700610fa5a38597a2b403dd900fa2b
Olena Reukova <kronverk01@gmail.com>
2023-07-07 09:07:02 +0100

    for now just using H2 and p heading structure
    I deleted the progress bar component because we are not sure about the concept of usage.

--- client/src/component/PullRequestCard.js
+++ client/src/component/PullRequestCard.js
@@ -1,8 +1,8 @@
-import { AppContext } from "../AppContext";
-import { useContext } from "react";
-import RoundProgressBar from "./RoundProgressBar";
+import "./PullRequestCard.css";
 
-const PullRequestCard = () => {
+
+
+export const PullRequestCard = () => {
   const createdPullRequests = 20;
   const requiredPullRequests = 20;
   const currentMilestone = "Data Base week 3";
@@ -13,21 +13,22 @@ const PullRequestCard = () => {
   const percentComplete = Math.floor((createdPullRequests / requiredPullRequests) * 100);
 
   return (
-    <div>
-      <h4>Pull Requests</h2>
+    <div className="pr_card">
+      <h2>Pull Requests</h2>
       <p>Your created pull requests: {createdPullRequests}</p>
       <p>Milestone: {currentMilestone}</p>
       <p>Required pull requests: {requiredPullRequests}</p>
-      <h5>You are at Milestone</h5>
-      <h4>Next Milestone: {nextMilestone}</h4>
+      <p>You are at Milestone</p>
+      <p>Next Milestone: {nextMilestone}</p>
       <p>Remaining days: {remainingDays}</p>
       <p>Required pull requests for next milestone: {nextMilestonePullRequests}</p>
       <p>You need {nextMilestonePullRequests - createdPullRequests} more pull requests in total</p>
       <p>You have created {percentComplete}% of the pull requests required to reach the next milestone</p>
-      <RoundProgressBar percentComplete={percentComplete} />
     </div>
   );
 };
+
+
 
 
 
