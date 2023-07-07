195073086323d4f28324f10b4463d5a2388a9518
Olena Reukova <kronverk01@gmail.com>
2023-07-07 16:34:29 +0100

    fixing bug

--- client/src/component/PullRequestCard.js
+++ client/src/component/PullRequestCard.js
@@ -7,31 +7,8 @@ export const PullRequestCard = ({
 	nextMilestone,
 	remainingDays,
 	nextMilestonePullRequests,
-	percentComplete
-  }) => {
-	return (
-	  <div className="pr_card">
-		<h2>Pull Requests</h2>
-		<p>Your created pull requests: {createdPullRequests}</p>
-		<p>Milestone: {currentMilestone}</p>
-		<p>Required pull requests: {requiredPullRequests}</p>
-		<p>You are at Milestone</p>
-		<p>Next Milestone: {nextMilestone}</p>
-		<p>Remaining days: {remainingDays}</p>
-		<p>Required pull requests for next milestone: {nextMilestonePullRequests}</p>
-		<p>You need {nextMilestonePullRequests - createdPullRequests} more pull requests in total</p>
-		<p>You have created {percentComplete}% of the pull requests required to reach the next milestone</p>
-	  </div>
-	);
-  };
-  
-  
-  
-  
-  
-  
-  
-
+	percentComplete,
+}) => {
 	return (
 		<div className="pr_card">
 			<h2>Pull Requests</h2>
