7c0fdd955c6797bf7083cfe19f841775099e872e
Olena Reukova <kronverk01@gmail.com>
2023-07-07 15:40:28 +0100

    PullRequestCard component with destructured props
    In this way, I can pass the required props directly to the PullRequestCard component when using it. It provides more flexibility and allows reusability of the component with different values for each prop.

--- client/src/component/PullRequestCard.js
+++ client/src/component/PullRequestCard.js
@@ -1,16 +1,36 @@
 import "./PullRequestCard.css";
 
-export const PullRequestCard = () => {
-	const createdPullRequests = 20;
-	const requiredPullRequests = 20;
-	const currentMilestone = "Data Base week 3";
-	const nextMilestone = "Final project";
-	const remainingDays = 17;
-	const nextMilestonePullRequests = 24;
-
-	const percentComplete = Math.floor(
-		(createdPullRequests / requiredPullRequests) * 100
+export const PullRequestCard = ({
+	createdPullRequests,
+	requiredPullRequests,
+	currentMilestone,
+	nextMilestone,
+	remainingDays,
+	nextMilestonePullRequests,
+	percentComplete
+  }) => {
+	return (
+	  <div className="pr_card">
+		<h2>Pull Requests</h2>
+		<p>Your created pull requests: {createdPullRequests}</p>
+		<p>Milestone: {currentMilestone}</p>
+		<p>Required pull requests: {requiredPullRequests}</p>
+		<p>You are at Milestone</p>
+		<p>Next Milestone: {nextMilestone}</p>
+		<p>Remaining days: {remainingDays}</p>
+		<p>Required pull requests for next milestone: {nextMilestonePullRequests}</p>
+		<p>You need {nextMilestonePullRequests - createdPullRequests} more pull requests in total</p>
+		<p>You have created {percentComplete}% of the pull requests required to reach the next milestone</p>
+	  </div>
 	);
+  };
+  
+  
+  
+  
+  
+  
+  
 
 	return (
 		<div className="pr_card">
