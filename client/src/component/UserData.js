a4e5c65a8ba01caeb5f3961cefbe78b55932a996
BoshraM <gbboshra@gmail.com>
2023-07-10 00:50:13 +0100

    milestone deatils added

--- client/src/component/UserData.js
+++ client/src/component/UserData.js
@@ -1,10 +1,18 @@
 import { useEffect, useState, useContext } from "react";
 import { AppContext } from "../AppContext";
+import milestoneDetails from "./../../data/milestones.json";
 
 export const UserData = () => {
 	const { userName } = useContext(AppContext);
-	const [currentMilestone, setCurrentMilestone] = useState(null);
 	const [cohortData, setCohortData] = useState({});
+	const [currentMilestoneName, setCurrentMilestoneName] = useState(null);
+	const [currentMilestoneData, setCurrentMilestoneData] = useState({
+		pulls: 0,
+		codewars: 9,
+	});
+
+
+	console.log(milestoneDetails);
 
 	useEffect(() => {
 		const fetchCohortData = async () => {
@@ -14,7 +22,6 @@ export const UserData = () => {
 					throw new Error("Couldn't fetch cohort's data");
 				}
 				const data = await res.json();
-				console.log(data);
 				setCohortData(data.milestoneData);
 			} catch (error) {
 				console.error("Error:", error);
@@ -25,28 +32,30 @@ export const UserData = () => {
 	}, [userName]);
 
 	const { start, html_css, js1_week2, js2_week1, js3_week3, react_week2, node_week2, db_week3, fp_week2 } = cohortData;
-	const newObject = { start, html_css, js1_week2, js2_week1, js3_week3, react_week2, node_week2, db_week3, fp_week2 };
+	const milestoneDeadlines = { start, html_css, js1_week2, js2_week1, js3_week3, react_week2, node_week2, db_week3, fp_week2 };
 
 
 	useEffect(() => {
 		const getMilestone = () => {
 			const currentDate = new Date().getTime();
 
-			Object.entries(newObject).forEach(([key, value]) => {
-				let newValue = new Date(value).getTime();
+			Object.entries(milestoneDeadlines).forEach(([key, value]) => {
+				let convertDate = new Date(value).getTime();
+				let timeDiff = currentDate - convertDate;
 
-				let difference = currentDate - newValue;
-				console.log("diff", difference);
-				if (difference < 0) {
-					console.log(currentMilestone);
-					return currentMilestone;
+				if (timeDiff <= 0) {
+					console.log("currentMilestone",currentMilestoneName);
+					setCurrentMilestoneData(milestoneDetails[currentMilestoneName]);
+					console.log("detailssss", currentMilestoneData);
+					return currentMilestoneName;
 				}
-				if (difference > 0) {
-					setCurrentMilestone(key);
+
+				if (timeDiff > 0) {
+					setCurrentMilestoneName(key);
 				}
 			});
 		};
 
 		getMilestone();
-	}, [newObject, currentMilestone]);
+	}, [milestoneDeadlines,currentMilestoneName, currentMilestoneData]);
 };
