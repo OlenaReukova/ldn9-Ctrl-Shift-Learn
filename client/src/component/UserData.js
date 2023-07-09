b735fbc38aa946c4a934037ffab1be1addf156d5
BoshraM <gbboshra@gmail.com>
2023-07-09 14:20:43 +0100

    getting current nmilestone depends on time

---
+++ client/src/component/UserData.js
@@ -0,0 +1,55 @@
+import { useEffect, useState, useContext } from "react";
+import { AppContext } from "../AppContext";
+
+export const Milestone = () => {
+	const { userName } = useContext(AppContext);
+	const [currentMilestone, setCurrentMilestone] = useState(null);
+	const [cohortData, setCohortData] = useState({});
+
+
+	useEffect(() => {
+		const fetchCohortData = async () => {
+			try {
+				const res = await fetch(`/api/trainees/search/${userName}`);
+				if (!res.ok) {
+					throw new Error("Couldn't fetch cohort's data");
+				}
+				const data = await res.json();
+				console.log(data);
+				setCohortData(data.milestoneData);
+			} catch (error) {
+				console.error("Error:", error);
+			}
+		};
+
+		fetchCohortData();
+	}, [userName]);
+
+	const { start, html_css, js1_week2, js2_week1, js3_week3, react_week2, node_week2, db_week3, fp_week2 } = cohortData;
+	const newObject = { start, html_css, js1_week2, js2_week1, js3_week3, react_week2, node_week2, db_week3, fp_week2 };
+
+
+	useEffect(() => {
+		const getMilestone = () => {
+			const currentDate = new Date().getTime();
+
+			Object.entries(cohortData).forEach(([key, value]) => {
+				let newValue = new Date(value).getTime();
+
+				let difference = currentDate - newValue;
+				console.log("diff", difference);
+				if (difference < 0) {
+					console.log(currentMilestone);
+					return currentMilestone;
+				}
+				if (difference > 0) {
+					setCurrentMilestone(key);
+				}
+			});
+		};
+
+		getMilestone();
+	}, [cohortData, currentMilestone]);
+
+	// Rest of the component code...
+};
