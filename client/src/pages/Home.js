37dc6296dc7e13f75783998f3745ba9f6b3149d5
MohammadSefidgar <108987757+MohammadSefidgar@users.noreply.github.com>
2023-07-10 14:39:25 +0100

    home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,9 @@
 import { User } from "../component/User";
-import { Footer } from "../component/Footer";
-import "../component/Footer.css";
+
 export const Home = () => {
 	return (
 		<main className="main_container" role="main">
 			<User />
-			<Footer />
 		</main>
 	);
 };
