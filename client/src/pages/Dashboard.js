982bf06ad4c1f2580f93e2c1e623291aab5fce8e
Olena Reukova <kronverk01@gmail.com>
2023-07-11 21:54:44 +0100

    fixing bugs

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -4,77 +4,104 @@ import { DashboardHero } from "../component/DashboardHero";
 import { CodeWarsCard } from "../component/CodeWarsCard";
 import { PullRequestCard } from "../component/PullRequestCard";
 import milestoneDetails from "./../../data/milestones.json";
-import { mandatoryCourswork } from "../../data/mandatoryCoursWork";
 
 export const Dashboard = () => {
-  const { userName, githubData } = useContext(AppContext);
-  const [cohortData, setCohortData] = useState({});
-  const [currentMilestoneName, setCurrentMilestoneName] = useState("");
-  const [nextMilestoneName, setNextMilestoneName] = useState("");
+	const { userName } = useContext(AppContext);
+	const [cohortData, setCohortData] = useState({});
+	const [currentMilestoneName, setCurrentMilestoneName] = useState("");
+	const [nextMilestoneName, setNextMilestoneName] = useState("");
 
-  console.log(githubData);
-  console.log(mandatoryCourswork);
-  const filteredPulls = githubData.items?.filter((pull) => {
-    const repoName = pull.url.replace("https://api.github.com/repos/CodeYourFuture/", "").split("/")[0].toLowerCase();
-    return mandatoryCourswork.map((item) => item.toLowerCase()).includes(repoName);
-  });
+	useEffect(() => {
+		const fetchCohortData = async () => {
+			try {
+				const res = await fetch(`/api/trainees/search/${userName}`);
+				if (!res.ok) {
+					throw new Error("Couldn't fetch cohort's data");
+				}
+				const data = await res.json();
+				setCohortData(data.milestoneData);
+			} catch (error) {
+				console.error("Error:", error);
+			}
+		};
+		fetchCohortData();
+	}, [userName]);
 
-  console.log(filteredPulls);
+	const {
+		start,
+		html_css,
+		js1_week2,
+		js2_week1,
+		js3_week3,
+		react_week2,
+		node_week2,
+		db_week3,
+		fp_week2,
+	} = cohortData;
 
-  useEffect(() => {
-    const fetchCohortData = async () => {
-      try {
-        const res = await fetch(`/api/trainees/search/${userName}`);
-        if (!res.ok) {
-          throw new Error("Couldn't fetch cohort's data");
-        }
-        const data = await res.json();
-        setCohortData(data.milestoneData);
-      } catch (error) {
-        console.error("Error:", error);
-      }
-    };
+	const cohortMilestoneDeadlines = useMemo(
+		() => ({
+			start,
+			html_css,
+			js1_week2,
+			js2_week1,
+			js3_week3,
+			react_week2,
+			node_week2,
+			db_week3,
+			fp_week2,
+		}),
+		[
+			start,
+			html_css,
+			js1_week2,
+			js2_week1,
+			js3_week3,
+			react_week2,
+			node_week2,
+			db_week3,
+			fp_week2,
+		]
+	);
 
-    fetchCohortData();
-  }, [userName]);
-  const { start, html_css, js1_week2, js2_week1, js3_week3, react_week2, node_week2, db_week3, fp_week2 } = cohortData;
-  const cohortMilestoneDeadlines = useMemo(() => (
-    { start, html_css, js1_week2, js2_week1, js3_week3, react_week2, node_week2, db_week3, fp_week2 }), [start, html_css, js1_week2, js2_week1, js3_week3, react_week2, node_week2, db_week3, fp_week2]);
+	useEffect(() => {
+		let indexOfNextMilestone = 0;
+		const cohortMilestoneDeadlinesArray = Object.entries(
+			cohortMilestoneDeadlines
+		);
+		indexOfNextMilestone = cohortMilestoneDeadlinesArray.findIndex(
+			([, value]) => {
+				const currentDate = new Date().getTime();
+				const timestampForValue = new Date(value).getTime();
+				return currentDate <= timestampForValue;
+			}
+		);
+		if (indexOfNextMilestone !== -1) {
+			const [currentMilestoneName] =
+				cohortMilestoneDeadlinesArray[indexOfNextMilestone - 1];
+			const [nextMilestoneName] =
+				cohortMilestoneDeadlinesArray[indexOfNextMilestone];
+			setCurrentMilestoneName(currentMilestoneName);
+			setNextMilestoneName(nextMilestoneName);
+		} else {
+			console.log("loading...");
+		}
+	}, [cohortMilestoneDeadlines]);
 
-  useEffect(() => {
-    let indexOfNextMilestone = 0;
-    const cohortMilestoneDeadlinesArray = Object.entries(cohortMilestoneDeadlines);
-    indexOfNextMilestone = cohortMilestoneDeadlinesArray.findIndex(([, value]) => {
-      const currentDate = new Date().getTime();
-      const timestampForValue = new Date(value).getTime();
-      return currentDate <= timestampForValue;
-    });
-
-    if (indexOfNextMilestone !== -1) {
-      const [currentMilestoneName] = cohortMilestoneDeadlinesArray[indexOfNextMilestone - 1];
-      const [nextMilestoneName] = cohortMilestoneDeadlinesArray[indexOfNextMilestone];
-
-      setCurrentMilestoneName(currentMilestoneName);
-      setNextMilestoneName(nextMilestoneName);
-    } else {
-      console.log("loading...");
-    }
-  }, [cohortMilestoneDeadlines]);
-
-  if (currentMilestoneName && nextMilestoneName) {
-    return (
-      <div className="dashboard">
-        <DashboardHero
-        />
-        <PullRequestCard data={{
-          pulls: milestoneDetails[nextMilestoneName].pulls,
-          codewars: milestoneDetails[nextMilestoneName].codewars,
-          name: nextMilestoneName,
-          deadline: cohortMilestoneDeadlines[nextMilestoneName],
-        }} />
-        <CodeWarsCard />
-      </div>
-    );
-  }
-
+	if (currentMilestoneName && nextMilestoneName) {
+		return (
+			<div className="dashboard">
+				<DashboardHero />
+				<PullRequestCard
+					data={{
+						pulls: milestoneDetails[nextMilestoneName].pulls,
+						codewars: milestoneDetails[nextMilestoneName].codewars,
+						name: nextMilestoneName,
+						deadline: cohortMilestoneDeadlines[nextMilestoneName],
+					}}
+				/>
+				<CodeWarsCard />
+			</div>
+		);
+	}
 };
