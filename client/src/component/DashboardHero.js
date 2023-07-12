c68b2ba89540c4163cdf2fc6859bfd93cc0fd42c
MohammadSefidgar <108987757+MohammadSefidgar@users.noreply.github.com>
2023-07-12 23:56:40 +0100

    Dashboard-Hero

--- client/src/component/DashboardHero.js
+++ client/src/component/DashboardHero.js
@@ -1,10 +1,13 @@
 import { useContext } from "react";
 import { AppContext } from "../AppContext";
-export const DashboardHero = ()=>{
-    const { userName } = useContext(AppContext);
-            return (
-              <div className="pr_card">
-                <h2>Hello {userName}!</h2>
-              </div>
-            );
-          };
+export const DashboardHero = () => {
+  const { userName, githubData, codewarsData } = useContext(AppContext);
+
+  return (
+    <div className="pr_card">
+      <h2>Hello {userName}!</h2>
+      <p>Submitted pull request: {githubData}</p>
+      <p>CodeWars Rank: {codewarsData}</p>
+    </div>
+  );
+};
