10a2c853d2d4e969e0c09df959d0aaf91b3ab860
zobeir-rigi <rigi.zobir@yahoo.com>
2023-07-13 05:38:35 +0100

    I didn't wrap it in form because form is not submiting so useless,error handing

--- client/src/component/Search.js
+++ client/src/component/Search.js
@@ -1,45 +1,62 @@
 import "./User.css";
-import { useContext } from "react";
+import { useContext,useState } from "react";
 import { AppContext } from "../AppContext";
+import { useNavigate } from "react-router-dom";
 
 export const Search = () => {
-    const { userName, setCodewarsData, setGitHubData } = useContext(AppContext);
-    const handleSearch = () => {
-        /////****GETTING CODEWARS DATA****///////
-        fetch(`https://www.codewars.com/api/v1/users/${userName}`)
-            .then((res) => {
-                if (!res.ok) {
-                    throw new Error(res.statusText);
-                }
-                return res.json();
-            })
-            .then((data) => {
-                return setCodewarsData(data);
-            })
-            .catch((err) => {
-                console.error(err);
-            });
-        ///////GETING GITHUB PULL REQUEST ON CYF REPOSITORI
-        fetch(`https://api.github.com/search/issues?q=is:pr%20author:${userName}%20user:codeyourfuture`)
-            .then((res) => {
-                if (!res.ok) {
-                    throw new Error(res.statusText);
-                }
-                return res.json();
-            })
-            .then((data) => {
-                return setGitHubData(data);
-            })
-            .catch((err) => {
-                console.error(err);
-            });
-    };
-    return (
-        <button className="user-button" onClick={() => handleSearch()} >search</button>
-    );
+	const { userName, setCodewarsData, setGitHubData } = useContext(AppContext);
+    const navigate = useNavigate();
+    const [error, setError] = useState(false);
+
+	const handleSearch = () => {
+		/////****GETTING CODEWARS DATA****///////
+		fetch(`https://www.codewars.com/api/v1/users/${userName}`)
+			.then((res) => {
+				if (!res.ok) {
+					throw new Error(res.statusText);
+				}
+				return res.json();
+			})
+			.then((data) => {
+                navigate("/result");
+				return setCodewarsData(data);
+
+			})
+			.catch((err) => {
+				console.error(err);
+                setError(true);
+                });
+                setTimeout(() => {
+                    setError(false);
+                  }, 3000);
+
+		///////GETING GITHUB PULL REQUEST ON CYF REPOSITORI
+		fetch(
+			`https://api.github.com/search/issues?q=is:pr%20author:${userName}%20user:codeyourfuture`
+		)
+			.then((res) => {
+				if (!res.ok) {
+					throw new Error(res.statusText);
+				}
+				return res.json();
+			})
+			.then((data) => {
+				return setGitHubData(data);
+			})
+			.catch((err) => {
+				console.error(err);
+                setError(true);
+			});
+
+	};
+	return (
+            <div>
+                <div>
+                    <button className="user-button" onClick={handleSearch}>
+                      Go for It
+                    </button>
+                </div>
+                        <div>{error && <p>User does not exist in the organization.</p>}</div>
+            </div>
+      );
 };
-
-
-
-
-
--- client/src/component/User.js
+++ client/src/component/User.js
@@ -2,7 +2,6 @@ import "./User.css";
 import { useContext } from "react";
 import { Search } from "../component/Search";
 import { AppContext } from "../AppContext";
-import { Link } from "react-router-dom";
 
 export const User = () => {
 	const { setUserName } = useContext(AppContext);
@@ -12,17 +11,19 @@ export const User = () => {
 	};
 
 	return (
-		<div className="user-container">
-			<label htmlFor="username">Your GitHub User Name</label>
-			<input
-				className="user-input"
-				type="text"
-				id="username"
-				onChange={handleInputChange}
-			/>
-			<Link to="/result">
+		<div>
+			<div className="user-container">
+				<label htmlFor="username">Your GitHub User Name</label>
+				<input
+					className="user-input"
+					type="text"
+					id="username"
+					onChange={handleInputChange}
+				/>
+			</div>
+			<div>
 				<Search />
-			</Link>
+			</div>
 		</div>
 	);
 };
