02ed05748c67dfffa6312d8b9b37d005c46d8a97
Olena Reukova <kronverk01@gmail.com>
2023-07-14 16:49:35 +0100

    changed font-size, padding, margin and font size for H1 in global css
    we need to centre circle progress bar in   recharts-responsive-container

--- client/src/component/MilestoneCard.js
+++ client/src/component/MilestoneCard.js
@@ -1,63 +1,55 @@
 import "./milestoneCard.css";
 import { CircleChart } from "./CircleChart";
 export const MilestoneCard = ({
-	time,
-	timeVerb,
-	data,
-	display,
+    time,
+    timeVerb,
+    data,
+    display,
 }) => {
-	const getCodewarsScore = (value) => {
-		let score = value === 8 ? 0 : value === 7 ? 20 : value === 6 ? 76 : value === 5 ? 229 : value === 5 ? 643 : 0;
-		return score;
-	};
-
-	const checkMilestone = (target, achieved) => (achieved < target ? "BEHIND" : achieved === target ? "AT" : achieved > target ? "BEYOND" : "");
-
-
-	return (
-		<div className="milestone__card-container">
-			<div className="milestone__title-container">
-				<p className="milestone-title">
-					<span>{time} Milestone</span>
-					<span>{data.name}</span>
-				</p>
-				<p>
-					Deadline {timeVerb} {data.deadline}
-				</p>
-			</div>
-			<div className="milestone__card-subcontainer">
-				<div className="factor-container">
-					<p className="factor-title">Pull Requests</p>
-					<p className="card-prgph" >
-						<span>TARGET:</span>
-						<span>{data.targetPulls}</span>
-					</p>
-					<p className="card-prgph">
-						<span>ACHIEVED:</span>
-						<span>{data.achievedPulls}</span>
-					</p>
-					<div>
-						<CircleChart targetScore={data.targetPulls} achievedscore={data.achievedPulls} />
-					</div>
-
-					<p style={{ display: display }}>{checkMilestone(data.targetPulls, data.achievedPulls)} Milestone</p>
-				</div>
-
-				<div className="factor-container">
-					<p className="factor_title">Codewars</p>
-					<p className="card-prgph">
-						<span>TARGET:</span>
-						<span>{data.targetRank}</span>
-					</p>
-					<p className="card-prgph">
-						<span>ACHIEVED:</span>
-						<span>{data.achievedRank} Kyu</span>
-					</p>
-					<CircleChart targetScore={getCodewarsScore(data.targetRank)} achievedscore={data.achievedScore} />
-					<p style={{ display: display }}>{checkMilestone(data.targetRank, data.achievedRank)} Milestone</p>
-
-				</div>
-			</div>
-		</div>
-	);
+    const getCodewarsScore = (value) => {
+        let score = value === 8 ? 0 : value === 7 ? 20 : value === 6 ? 76 : value === 5 ? 229 : value === 5 ? 643 : 0;
+        return score;
+    };
+    const checkMilestone = (target, achieved) => (achieved < target ? "Behind" : achieved === target ? "At" : achieved > target ? "Beyond" : "");
+    return (
+        <div className="milestone__card-container">
+            <div className="milestone__title-container">
+                <p className="milestone-title">
+                    <span>{time} Milestone</span>
+                    <span>{data.name.toUpperCase()}</span>
+                </p>
+                <p className="deadline-prgph">
+                    Deadline {timeVerb} {data.deadline}
+                </p>
+            </div>
+            <div className="milestone__card-subcontainer">
+                <div className="factor-container">
+                    <p className="factor-title">Pull Requests</p>
+                    <p className="card-prgph" >
+                        <span>Target:</span>
+                        <span className="card-prgph-number">{data.targetPulls}</span>
+                    </p>
+                    <p className="card-prgph">
+                        <span>Achieved:</span>
+                        <span className="card-prgph-number">{data.achievedPulls}</span>
+                    </p>
+                    <CircleChart targetScore={data.targetPulls} achievedscore={data.achievedPulls} />
+                    <p style={{ display: display }}>{checkMilestone(data.targetPulls, data.achievedPulls)} Milestone</p>
+                </div>
+                <div className="factor-container">
+                    <p className="factor-title">Codewars</p>
+                    <p className="card-prgph">
+                        <span>Target:</span>
+                        <span className="card-prgph-number">{data.targetRank} <span className="kyu">Kyu</span></span>
+                    </p>
+                    <p className="card-prgph">
+                        <span>Achieved:</span>
+                        <span className="card-prgph-number">{data.achievedRank} <span className="kyu">Kyu</span></span>
+                    </p>
+                    <CircleChart targetScore={getCodewarsScore(data.targetRank)} achievedscore={data.achievedScore} />
+                    <p style={{ display: display }}>{checkMilestone(data.targetRank, data.achievedRank)} Milestone</p>
+                </div>
+            </div>
+        </div>
+    );
 };
