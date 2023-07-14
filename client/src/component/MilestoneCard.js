9eff44df6a99550d2e14c75be2b0eedc407d019c
BoshraM <gbboshra@gmail.com>
2023-07-14 01:37:03 +0100

    style update with figma

--- client/src/component/MilestoneCard.js
+++ client/src/component/MilestoneCard.js
@@ -4,6 +4,7 @@ export const MilestoneCard = ({
 	time,
 	timeVerb,
 	data,
+	display,
 }) => {
 	const getCodewarsScore = (value) => {
 		let score = value === 8 ? 0 : value === 7 ? 20 : value === 6 ? 76 : value === 5 ? 229 : value === 5 ? 643 : 0;
@@ -16,46 +17,45 @@ export const MilestoneCard = ({
 	return (
 		<div className="milestone__card-container">
 			<div className="milestone__title-container">
-				<h2>
-					<span>{time} Milestone:</span>
+				<p className="milestone-title">
+					<span>{time} Milestone</span>
 					<span>{data.name}</span>
-				</h2>
+				</p>
 				<p>
 					Deadline {timeVerb} {data.deadline}
 				</p>
 			</div>
 			<div className="milestone__card-subcontainer">
 				<div className="factor-container">
-					<h2>Pull Requests</h2>
-					<p>
-						<span>TARGET</span>
+					<p className="factor-title">Pull Requests</p>
+					<p className="card-prgph" >
+						<span>TARGET:</span>
 						<span>{data.targetPulls}</span>
 					</p>
-					<p>
-						<span>ACHIEVED</span>
+					<p className="card-prgph">
+						<span>ACHIEVED:</span>
 						<span>{data.achievedPulls}</span>
 					</p>
 					<div>
 						<CircleChart targetScore={data.targetPulls} achievedscore={data.achievedPulls} />
 					</div>
 
-					<h2>{checkMilestone(data.targetPulls, data.achievedPulls)} Milestone</h2>
+					<p style={{ display: display }}>{checkMilestone(data.targetPulls, data.achievedPulls)} Milestone</p>
 				</div>
 
 				<div className="factor-container">
-					<h2>Codewars</h2>
-					<p>
-						<span>TARGET</span>
+					<p className="factor_title">Codewars</p>
+					<p className="card-prgph">
+						<span>TARGET:</span>
 						<span>{data.targetRank}</span>
 					</p>
-					<p>
-						<span>ACHIEVED</span>
-						<span>{data.achievedRank}</span>
+					<p className="card-prgph">
+						<span>ACHIEVED:</span>
+						<span>{data.achievedRank} Kyu</span>
 					</p>
-					<div>
 					<CircleChart targetScore={getCodewarsScore(data.targetRank)} achievedscore={data.achievedScore} />
-					</div>
-					<h2>{checkMilestone(data.targetRank, data.achievedRank)} Milestone</h2>
+					<p style={{ display: display }}>{checkMilestone(data.targetRank, data.achievedRank)} Milestone</p>
+
 				</div>
 			</div>
 		</div>
