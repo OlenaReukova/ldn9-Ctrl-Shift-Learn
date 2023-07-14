e6f285436f509a7690ce5b7546f7aac3ae6c6611
zobeir-rigi <rigi.zobir@yahoo.com>
2023-07-14 01:33:03 +0100

    header updated

--- client/src/component/Header.js
+++ client/src/component/Header.js
@@ -1,21 +1,6 @@
-import { useLocation } from "react-router-dom";
-import { Link } from "react-router-dom";
 import "./header.css";
 
 export const Header = () => {
-	const location = useLocation();
-	const adminPages = location.pathname.startsWith("/admin");
-	const home = location.pathname === "/";
-	const about = location.pathname === "/about";
-	const isCohortsPage =
-		location.pathname === "/admin/cohorts" ||
-		location.pathname === "/admin/cohorts/";
-
-	const isCohortDetailsPage =
-		!location.pathname.includes("addcohort") &&
-		!location.pathname.includes("updateTrainee") &&
-		!location.pathname.includes("addtrainee");
-
 	return (
 		<header>
 			<img
@@ -23,47 +8,14 @@ export const Header = () => {
 				src="https://syllabus.codeyourfuture.io/img/logo.png"
 				alt="CYF"
 			/>
-
-			<ul className="navbar">
-				{!home && (
-					<li>
-						<Link className="link" to="/">
-							Home
-						</Link>
-					</li>
-				)}
-				{adminPages && (
-					<>
-						{!isCohortsPage && (
-							<li>
-								<Link className="link" to="/admin/cohorts">
-								</Link>
-							</li>
-						)}
-						{isCohortsPage && (
-							<li>
-								<Link className="link" to="/admin/cohorts/addcohort">
-									Add New Cohort
-								</Link>
-							</li>
-						)}
-						{!isCohortsPage && isCohortDetailsPage && (
-							<li>
-								<Link className="link" to="/admin/cohorts/:id/addtrainee">
-									Add New Trainee
-								</Link>
-							</li>
-						)}
-					</>
-				)}
-				{!about && (
-					<li>
-						<Link className="link" to="/about">
-							About
-						</Link>
-					</li>
-				)}
-			</ul>
+			<div className="topic">
+				<span className="cyf">CYF</span>{" "}
+				<span
+					className="progress">
+					progress tracker
+				</span>
+			</div>
+			<p className="team-name">Ctrl + Shift + Learn</p>
 		</header>
 	);
 };
