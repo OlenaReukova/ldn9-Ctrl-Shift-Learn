26361434247ca4a5fdb265fa3a59d63076af6fa2
Olena Reukova <kronverk01@gmail.com>
2023-07-15 05:13:58 +0100

    css mobile for dashboard

--- client/src/component/DashboardHero.js
+++ client/src/component/DashboardHero.js
@@ -5,9 +5,14 @@ export const DashboardHero = (data) => {
   const { userName } = useContext(AppContext);
   return (
     <div className="pr_card">
-      <p className="dashboardHero-title">Hi, {userName} London9</p>
-      <p className="hero-prgph">Submitted pull requests: <span className="card-prgph-number">{data.achievedPulls}</span></p>
+      <div className="hero-title">
+      <p className="dashboardHero-title">Hi, {userName}</p>
+      <p className="dashboardHero-title-cohort">London9</p>
+      </div>
+      <div className="hero-subcontainer">
+      <p className="hero-prgph">Submitted PRs: <span className="card-prgph-number">{data.achievedPulls}</span></p>
       <p className="hero-prgph">CodeWars Rank: <span className="card-prgph-number">{data.achievedRank}</span> kyu</p>
+      </div>
       <p className="hero-details">Scroll down to see more details</p>
     </div>
   );
