7dd2a1a802cad30169e2e0ad501789885e81661d
BoshraM <gbboshra@gmail.com>
2023-07-15 12:20:01 +0100

    fix header image

--- client/src/component/Header.js
+++ client/src/component/Header.js
@@ -1,19 +1,21 @@
 import "./header.css";
 import { Link } from "react-router-dom";
+import imageSrc from "../../data/imge/cyf-logo.png";
 export const Header = () => {
 	return (
 		<header>
-			<Link to={"/"}>
-				<img
-					className="logo"
-					src="https://syllabus.codeyourfuture.io/img/logo.png"
-					alt="CYF"
-				/>
-			</Link>
+
 			<p className="topic">
 				<span className="cyf">CYF</span>{" "}
 				<span className="progress">progress tracker</span>
 			</p>
+			<Link to={"/"}>
+				<img
+					className="logo"
+					src={imageSrc}
+					alt="CYF"
+				/>
+			</Link>
 			<p className="team-name">Ctrl + Shift + Learn</p>
 		</header>
 	);
